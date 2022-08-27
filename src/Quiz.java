a2daaa3a8de79433238c7efeddf87ff4a54630cf
poul666 <hapko3831@gmail.com>
2022-08-27 14:13:28 +0700

    first commit

---
+++ src/Quiz.java
@@ -0,0 +1,10 @@
+public class Quiz {
+
+    public static void main(String[] args) {
+        System.out.println("Я изучаю Java");
+        System.out.println("Я" + "   " + "изучаю Java");
+        System.out.println("Я изучаю");
+        System.out.println("Я изучаю");
+        System.out.println("Я изучаю");
+    }
+}
